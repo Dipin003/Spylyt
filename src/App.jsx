7a07edd322f361d232d3ba14c675f91c1b87f022
Dipin003 <kharayatdipin@gmail.com>
2026-01-09 09:36:02 +0530

    add HeroSection with GSAP SplitText intro and scroll-based animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
+
   return (
     <main>
       <Navbar />
       <HeroSection />
+      <div className='h-dvh border-red-900'>
+
+      </div>
     </main>
   )
 }
